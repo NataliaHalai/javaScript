106989abf6c4727a836b20e942ffa311f00bcb54
natalia <atlant.kovalyova@gmail.com>
2024-02-05 21:00:12 +0200

    5022014

---
+++ js/script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+//First task
+// let name = "Іван";
+// let admin = "Іван";
+// alert(admin);
+
+
+//second
+
+let cityName = "Київ";
+let currentUsername;
+currentUsername = "Ольга";
+console.log(cityName);
+
+let firstNum = 5;
+console.log(typeof (5));
+
+console.log(typeof ("hgfjghj"));
+console.log(typeof (true));
+console.log(typeof (5474365783465784768578678567878));
+console.log(typeof (fjrhg6676788787));
+console.log(typeof (0));
+
+
+let storeName = "hm";
+
+const storeDescription = {
+    budget: 10000,
+    employees: ["Pernilla", "Elsa", "Stella"],
+    productsPrise: {
+        pants: 200,
+        hat: 300
+    },
+    open: true
+    
+};
+console.log(storeDescription.productsPrise.pants);
+console.log(storeDescription.employees[0]);
+
+
+
+
+
